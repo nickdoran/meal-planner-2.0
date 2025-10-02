a4075dbea85945eb00f032f39a32afa6a72f8fc6
nickdoran <nickdoran6@gmail.com>
2025-10-02 14:18:25 -0400

    Commit 1 - Intial Commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <div>
+      <h2 className="text-center p-4 bg-[#1E352F] text-gray-400 font-inter">
+        © 2024 Meal Planner. All rights reserved.
+      </h2>
+    </div>
+  );
+};
+
+export default Footer;
---
+++ src/components/Layout.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import Navbar from "./Navbar.jsx";
+import { Outlet } from "react-router-dom";
+import Footer from "./Footer.jsx";
+const Layout = () => {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
