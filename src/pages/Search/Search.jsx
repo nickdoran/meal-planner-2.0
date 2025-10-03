e79187c3869849153c4aa89de51f3d72cfa640ac
Nicklas Doran <you@example.com>
2025-10-03 18:53:01 -0400

    feat: added better ui for search results, and created no search card

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -12,7 +12,7 @@ const Search = () => {
       <div className="max-w-3xl mx-auto mb-12">
         <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
       </div>
-      <div className="mx-auto px-4 mb-8 sm:px-6 lg:px-8 w-full max-w-7xl">
+      <div className=" flex flex-1 mx-auto px-4 mb-8 sm:px-6 lg:px-8 w-full max-w-7xl">
         <SearchResults searchTerm={searchTerm} />
       </div>
     </div>
