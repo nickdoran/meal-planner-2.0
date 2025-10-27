8d2e0fa198a96eee82754258a1452d6c9a6fdfdc
nickdoran <nickdoran6@gmail.com>
2025-10-27 17:10:27 -0400

    created file: created a vercel config file

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react(), tailwindcss()],
-    base: "/meal-planner-2.0/",
+    build: {
+        outDir: "dist",
+        sourcemap: false,
+    },
 });
