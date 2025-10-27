27bd2cdbef82420ba181f737c85b7f4ba89ed21b
Nicklas Doran <you@example.com>
2025-10-27 17:18:27 -0400

    fix: trying to fix image loading

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -5,90 +5,91 @@ import { useSearchParams, useNavigate, Link } from "react-router-dom";
 import Modal from "../pages/Search/Modal";
 
 const Card = ({ meal }) => {
-  const [visible, setVisible] = useState(false);
-  const [showModal, setShowModal] = useState(false);
-  const [searchParams] = useSearchParams();
-  const day = searchParams.get("day");
-  const section = searchParams.get("section");
-  const navigate = useNavigate();
+    const [visible, setVisible] = useState(false);
+    const [showModal, setShowModal] = useState(false);
+    const [searchParams] = useSearchParams();
+    const day = searchParams.get("day");
+    const section = searchParams.get("section");
+    const navigate = useNavigate();
 
-  const onClose = () => {
-    setShowModal(false);
-  };
+    const onClose = () => {
+        setShowModal(false);
+    };
 
-  const handleAdd = () => {
-    if (day && section) {
-      const raw = localStorage.getItem("planner");
-      const plannerData = raw ? JSON.parse(raw) : {};
+    const handleAdd = () => {
+        if (day && section) {
+            const raw = localStorage.getItem("planner");
+            const plannerData = raw ? JSON.parse(raw) : {};
 
-      if (!plannerData[day]) plannerData[day] = {};
-      plannerData[day][section] = {
-        idMeal: meal.idMeal,
-        strMeal: meal.strMeal,
-        strMealThumb: meal.strMealThumb,
-        strCategory: meal.strCategory,
-        strArea: meal.strArea,
-      };
-      localStorage.setItem("planner", JSON.stringify(plannerData));
-      localStorage.setItem("lastUpdated", new Date().toISOString());
-      navigate(`/planner`);
-    } else {
-      setShowModal(true); // a flag to show if their should me a modal.
-    }
-  };
+            if (!plannerData[day]) plannerData[day] = {};
+            plannerData[day][section] = {
+                idMeal: meal.idMeal,
+                strMeal: meal.strMeal,
+                strMealThumb: meal.strMealThumb,
+                strCategory: meal.strCategory,
+                strArea: meal.strArea,
+            };
+            localStorage.setItem("planner", JSON.stringify(plannerData));
+            localStorage.setItem("lastUpdated", new Date().toISOString());
+            navigate(`/planner`);
+        } else {
+            setShowModal(true); // a flag to show if their should me a modal.
+        }
+    };
 
-  useEffect(() => {
-    // Trigger fade-in after mount
-    setTimeout(() => setVisible(true), 50);
-  }, []);
+    useEffect(() => {
+        // Trigger fade-in after mount
+        setTimeout(() => setVisible(true), 50);
+    }, []);
 
-  return (
-    <>
-      <Link
-        to={day ? "#" : `/mealdetails/${meal.idMeal}`}
-        className={`card-transition transform rounded-2xl shadow-lg flex flex-col justify-start items-start bg-transparent ${
-          visible ? "opacity-100" : "opacity-0"
-        }`}
-      >
-        <img
-          src={meal.strMealThumb}
-          alt={meal.strMeal}
-          loading="lazy"
-          className="w-full rounded-t-2xl border-b-emerald-500 border-4 border-x-[335145] border-x-transparent border-t-transparent"
-        />
-        <div className="w-full bg-slate-100 rounded-b-2xl flex-1 flex flex-col justify-between">
-          <div className=" flex w-full justify-between items-end px-4 pt-0.5 pb-4">
-            <div>
-              <h3 className="font-bold text-black">{meal.strMeal}</h3>
-              <p className="text-sm text-gray-400">
-                {meal.strCategory} · {meal.strArea}
-              </p>
-            </div>
-          </div>
-          <div className="flex w-full justify-between items-end px-3 pb-3 rounded-b-2xl">
-            <button
-              onClick={(e) => {
-                e.preventDefault();
-                e.stopPropagation();
-                handleAdd();
-              }}
-              className="bg-emerald-900 text-white rounded-lg py-2 px-4 hover:bg-emerald-800 transition-color duration-200"
+    return (
+        <>
+            <Link
+                to={day ? "#" : `/mealdetails/${meal.idMeal}`}
+                className={`card-transition transform rounded-2xl shadow-lg flex flex-col justify-start items-start bg-transparent ${
+                    visible ? "opacity-100" : "opacity-0"
+                }`}
             >
-              Add to Planner
-            </button>
-            <Star
-              onClick={(e) => {
-                e.stopPropagation();
-                e.preventDefault();
-              }}
-              className="w-8 h-8 stroke-yellow-500 fill-none hover:fill-yellow-500 cursor-pointer"
-            />
-          </div>
-        </div>
-      </Link>
-      {showModal && <Modal meal={meal} onClose={onClose} />}
-    </>
-  );
+                <img
+                    src={meal.strMealThumb}
+                    alt={meal.strMeal}
+                    className="w-full rounded-t-2xl border-b-emerald-500 border-4 border-x-[335145] border-x-transparent border-t-transparent"
+                />
+                <div className="w-full bg-slate-100 rounded-b-2xl flex-1 flex flex-col justify-between">
+                    <div className=" flex w-full justify-between items-end px-4 pt-0.5 pb-4">
+                        <div>
+                            <h3 className="font-bold text-black">
+                                {meal.strMeal}
+                            </h3>
+                            <p className="text-sm text-gray-400">
+                                {meal.strCategory} · {meal.strArea}
+                            </p>
+                        </div>
+                    </div>
+                    <div className="flex w-full justify-between items-end px-3 pb-3 rounded-b-2xl">
+                        <button
+                            onClick={(e) => {
+                                e.preventDefault();
+                                e.stopPropagation();
+                                handleAdd();
+                            }}
+                            className="bg-emerald-900 text-white rounded-lg py-2 px-4 hover:bg-emerald-800 transition-color duration-200"
+                        >
+                            Add to Planner
+                        </button>
+                        <Star
+                            onClick={(e) => {
+                                e.stopPropagation();
+                                e.preventDefault();
+                            }}
+                            className="w-8 h-8 stroke-yellow-500 fill-none hover:fill-yellow-500 cursor-pointer"
+                        />
+                    </div>
+                </div>
+            </Link>
+            {showModal && <Modal meal={meal} onClose={onClose} />}
+        </>
+    );
 };
 
 export default Card;
