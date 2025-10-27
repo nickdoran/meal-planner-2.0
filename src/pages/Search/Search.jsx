d2ca452381f599f8a8817ed16750ad2e73f6dcfc
Nicklas Doran <you@example.com>
2025-10-27 17:41:16 -0400

    refactored card to possibly fix images

--- src/pages/Search/Search.jsx
+++ src/pages/Search/Search.jsx
@@ -3,19 +3,24 @@ import SearchBar from "../../components/SearchBar.jsx";
 import SearchResults from "./SearchResults.jsx";
 
 const Search = () => {
-  const [searchTerm, setSearchTerm] = useState("");
+    const [searchTerm, setSearchTerm] = useState("");
 
-  return (
-    <div className="flex-1 flex flex-col">
-      <h1 className="mt-12 mb-6 mx-auto text-center">Search For Any Meal</h1>
-      <div className="max-w-3xl mx-auto mb-12">
-        <SearchBar searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
-      </div>
-      <div className=" flex flex-1 mx-auto px-4 mb-8 sm:px-6 lg:px-8 w-full max-w-7xl">
-        <SearchResults searchTerm={searchTerm} />
-      </div>
-    </div>
-  );
+    return (
+        <div className="flex-1 flex flex-col">
+            <h1 className="mt-12 mb-6 mx-auto text-center">
+                Search For Any Meal
+            </h1>
+            <div className="max-w-3xl mx-auto mb-12">
+                <SearchBar
+                    searchTerm={searchTerm}
+                    setSearchTerm={setSearchTerm}
+                />
+            </div>
+            <div className=" flex flex-1 justify-center mx-auto px-4 mb-8 sm:px-6 lg:px-8 w-full max-w-7xl">
+                <SearchResults searchTerm={searchTerm} />
+            </div>
+        </div>
+    );
 };
 
 export default Search;
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
+    plugins: [react(), tailwindcss()],
     base: "/",
     build: {
         outDir: "dist",
         assetsDir: "assets",
         sourcemap: false,
-    },
-    css: {
-        transformer: "postcss",
     },
 });
