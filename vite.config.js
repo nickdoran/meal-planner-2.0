f4136f49a53fa0e7bb029b2d8c14ad52a90a7a89
Nicklas Doran <you@example.com>
2025-10-27 17:15:25 -0400

    Added ite config and fix build

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
+    plugins: [react()],
+    base: "/",
     build: {
         outDir: "dist",
+        assetsDir: "assets",
         sourcemap: false,
+    },
+    css: {
+        transformer: "postcss",
     },
 });
